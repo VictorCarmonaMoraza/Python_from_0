1be6023efb61c94b90fcc3f3f4bd26040dc189a6
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 08:05:59 +0100

    17-Tipos Booleanos (bool)

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
-#Cadena (string)
-miGrupoFavorito = "El mago de oz"
-comentario = "La mejor banda"
-#Concatenacion
-print("Mi grupo favorito es: "+miGrupoFavorito+ " "+ comentario)
+# Tipos bool (boolean)
+miVariable = False
+print(miVariable)
 
-numero1 = "1"
-numero2="2"
-print(numero1+numero2)
-print("Suma = ",int(numero1) + int(numero2))
+miVariable = 1 > 2
+print(miVariable)
 
-
-numero1 = 1
-numero2 =2
-print(numero1+numero2)
+if miVariable:
+    print("El resultado fue verdadero")
+else:
+    print("El resultado fue Falso")
