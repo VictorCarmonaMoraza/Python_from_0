5f76e10869921b649fbc80b6a15a9e06a97ebfba
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 07:40:13 +0100

    12-Ejercicio de variables

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-x =10
-y=2
-z =x + y
+nombre = "Juan Perez"
+telefono = 9563251
+email = "v@gmail.com"
 
-print(x)
-print(y)
-print(z)
-
-#Cada vez que ejeuctamos la direccion de memoria cambia
-print("Direccion de memoria---->",id(x))
-print("Direccion de memoria---->",id(y))
-print("Direccion de memoria---->",id(z))
+print(nombre)
+print(telefono)
+print(email)
