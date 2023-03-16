20caa083af8f11ef074ad3484f67bd176836dd05
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 08:02:13 +0100

    16-mas temas d emajeo de cadenas

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 #Cadena (string)
-miGrupoFavorito = "Los brvaos " +" "+ "y El mago de oz"
+miGrupoFavorito = "El mago de oz"
+comentario = "La mejor banda"
 #Concatenacion
-print("Mi grupo favorito es: "+miGrupoFavorito)
+print("Mi grupo favorito es: "+miGrupoFavorito+ " "+ comentario)
+
+numero1 = "1"
+numero2="2"
+print(numero1+numero2)
+print("Suma = ",int(numero1) + int(numero2))
+
+
+numero1 = 1
+numero2 =2
+print(numero1+numero2)
