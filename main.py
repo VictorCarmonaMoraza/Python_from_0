8e1a985fbde71dc92aa5a62b21a2cf8f094b9d70
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 19:14:11 +0100

    72-Paso de argumentos a funciones

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Funciones
-def mi_Funcion():
-    print('Hola mundo')
+def mi_Funcion(nombre,apellido):
+    print('Nombre: '+nombre+ ' Apellido '+ apellido)
+    print(f'Nombre: {nombre}, Apellido: {apellido}')
 
 
-mi_Funcion()
+mi_Funcion('juan','Carmona')
