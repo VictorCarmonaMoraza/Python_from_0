60d06d90efe2df10baba402d74984cf5f52fdb71
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 18:55:57 +0100

    25-Operadores aritmeticos 2

--- main.py
+++ main.py
@@ -4,3 +4,32 @@ operandoB = 2
 suma = operandoA + operandoB
 print(f'Resultado suma: {suma}')
 
+operandoA = 3
+operandoB = 2
+resta = operandoA - operandoB
+print(f'Resultado resta: {resta}')
+
+operandoA = 3
+operandoB = 2
+multiplicacion = operandoA * operandoB
+print(f'Resultado multiplicacion: {multiplicacion}')
+
+operandoA = 3
+operandoB = 2
+division = operandoA / operandoB
+print(f'Resultado division: {division}')
+
+operandoA = 3
+operandoB = 2
+division2 = operandoA // operandoB
+print(f'Resultado division2: {division2}')
+
+operandoA = 3
+operandoB = 2
+modulo = operandoA % operandoB
+print(f'Resultado modulo: {modulo}')
+
+operandoA = 3
+operandoB = 2
+exponente = operandoA % operandoB
+print(f'Resultado exponente: {exponente}')
