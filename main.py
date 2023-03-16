dba9ad6907cf36590a00b95936adab8269c687fa
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 19:02:48 +0100

    26-Ejercicio Rectangulo

--- main.py
+++ main.py
@@ -1,35 +1,9 @@
+alto = float(input("Dame el alto: "))
+ancho = float(input("Dame el ancho: "))
 
-operandoA = 3
-operandoB = 2
-suma = operandoA + operandoB
-print(f'Resultado suma: {suma}')
+Area  = alto * ancho
+Perimetro = (alto + ancho) * 2
 
-operandoA = 3
-operandoB = 2
-resta = operandoA - operandoB
-print(f'Resultado resta: {resta}')
 
-operandoA = 3
-operandoB = 2
-multiplicacion = operandoA * operandoB
-print(f'Resultado multiplicacion: {multiplicacion}')
-
-operandoA = 3
-operandoB = 2
-division = operandoA / operandoB
-print(f'Resultado division: {division}')
-
-operandoA = 3
-operandoB = 2
-division2 = operandoA // operandoB
-print(f'Resultado division2: {division2}')
-
-operandoA = 3
-operandoB = 2
-modulo = operandoA % operandoB
-print(f'Resultado modulo: {modulo}')
-
-operandoA = 3
-operandoB = 2
-exponente = operandoA % operandoB
-print(f'Resultado exponente: {exponente}')
+print("El area es :",Area)
+print("El perimetro es: ", Perimetro)
