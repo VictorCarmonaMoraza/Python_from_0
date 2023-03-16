f568f2634ff6d9312b4479d09d7bef1554e438ac
Victor <VictorCarmona9035@hotmail.com>
2023-03-16 07:36:22 +0100

    10-Direccion de memoria y variables en Python

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ z =x + y
 print(x)
 print(y)
 print(z)
+
+#Cada vez que ejeuctamos la direccion de memoria cambia
+print("Direccion de memoria---->",id(x))
+print("Direccion de memoria---->",id(y))
+print("Direccion de memoria---->",id(z))
