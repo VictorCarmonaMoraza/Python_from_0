158048c5aa566f0c2359895b725773ff19fceb28
Victor <VictorCarmona9035@hotmail.com>
2023-03-17 21:40:25 +0100

    81-Distintos tipos de datos como argumentos en python

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 
-# Argumentos variables en argumentos
-def listarTerminos(**terminos):
-    for llave, valor in terminos.items():
-        print(f'{llave} : {valor}')
 
+def desplegarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
 
-#listarTerminos(IDE='Interprete', PK='Primary key')
-listarTerminos(IDE='Interprete', PK='Primary key')
+#Itera todos los elementos
+nombres = ['Juan','Carla','Guillermo']
+desplegarNombres(nombres)
 
+#Itera todas las letras del nombre cuando solo es una variable
+desplegarNombres('Carlos')
+#Tupla
+desplegarNombres((10, 11))
+#Lista
+desplegarNombres([10, 11])
