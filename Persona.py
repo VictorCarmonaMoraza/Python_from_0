7d3558d1dee5a164fb35bb191c5a1148a300143d
Victor <VictorCarmona9035@hotmail.com>
2023-03-20 19:32:02 +0100

    95-Metodos de instancia

--- Persona.py
+++ Persona.py
@@ -5,20 +5,15 @@ class Persona:
     self.apellido = apelli
     self.edad = age
 
-
+  #Metodo de nuestra clase
+  def mostrar_detalle(self):
+    print(f'Persona : {self.nombre} {self.apellido} {self.edad}')
 
 
 #Creamos un objeto de esta clase
 persona1 = Persona('Victor','Carmona', 45)
-print(persona1.nombre)
-print(persona1.apellido)
-print(persona1.edad)
-#print(type(Persona))
-
-persona1.nombre = ' Juan Carlos'
-persona1.apellido = 'Juarez'
-persona1.edad =25
-print(f'Objeto Persona 1:{persona1.nombre} {persona1.apellido} {persona1.edad}')
+persona1.mostrar_detalle()
 
 persona2 = Persona('jorge','Perez',2515)
-print(f'Objeto Persona 2:{persona2.nombre} {persona2.apellido} {persona2.edad}')
+persona2.mostrar_detalle()
+
