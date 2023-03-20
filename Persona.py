592de068c09f46b6ebc65f595da3018b18698b46
Victor <VictorCarmona9035@hotmail.com>
2023-03-20 19:16:35 +0100

    94-Modificar atributos de un objeto

--- Persona.py
+++ Persona.py
@@ -15,5 +15,10 @@ print(persona1.apellido)
 print(persona1.edad)
 #print(type(Persona))
 
+persona1.nombre = ' Juan Carlos'
+persona1.apellido = 'Juarez'
+persona1.edad =25
+print(f'Objeto Persona 1:{persona1.nombre} {persona1.apellido} {persona1.edad}')
+
 persona2 = Persona('jorge','Perez',2515)
 print(f'Objeto Persona 2:{persona2.nombre} {persona2.apellido} {persona2.edad}')
