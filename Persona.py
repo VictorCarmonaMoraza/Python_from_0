32cd86dbfa68b9ed5048c3db86c7accbd7ddd0fd
Victor <VictorCarmona9035@hotmail.com>
2023-03-20 20:56:44 +0100

    105-Metodo get y set

--- Persona.py
+++ Persona.py
@@ -4,18 +4,35 @@ class Persona:
 
   """
   def __init__(self, nom, apelli, age):
-    self.__nombre = nom
+    self._nombre = nom
     self.apellido = apelli
     self.edad = age
 
+  @property
+  def nombre(self):
+    print('Llamando metodo get nombre')
+    return self._nombre
+
+  @nombre.setter
+  def nombre(self, nombre):
+    print('LLamando metodo set nombre', nombre)
+    self._nombre = nombre
+
   #Metodo de nuestra clase
   def mostrar_detalle(self):
-    print(f'Persona : {self.__nombre} {self.apellido} {self.edad}')
+    print(f'Persona : {self._nombre} {self.apellido} {self.edad}')
 
 
 #Creamos un objeto de esta clase
 #Forma 1
 persona1 = Persona('Adris','Garcia', 45)
-persona1.mostrar_detalle()
+#persona1.mostrar_detalle()
+#LLamada al metodo getter
+print(persona1.nombre)
+
+#LLamada al metodo setter
+persona1.nombre = 'Juan Carlos'
+print(persona1.nombre)
+
 
 
