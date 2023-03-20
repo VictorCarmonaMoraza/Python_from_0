47fd57806a90cc39ec18e46c7a4b08745631acbc
Victor <VictorCarmona9035@hotmail.com>
2023-03-20 18:05:35 +0100

    88-Ejercicio convertidor de temperatura

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-
-pagoSinImpuestos = int(input("Ingrese pago sin impuestos:"))
-montoDelInpuesto = int(input("Monto del inpuestos:"))
-
-def calcular_total_pago(PagoSinInpuestosParam, MontoDelImpuestoParam):
-    pagoTotal = pagoSinImpuestos + pagoSinImpuestos*(montoDelInpuesto/100)
-    return pagoTotal
+#Convertidor de temperatudas
+#Convierte de Celcius a Farenheit
+def gradosCelciusA_Farenheit(gradoCelcius):
+    temperaturaFarenheit = (gradoCelcius *1.8) +32
+    return temperaturaFarenheit
 
 
-total = calcular_total_pago(pagoSinImpuestos,montoDelInpuesto)
-print('Pago con impuesto: ',total)
+#Convierte de Farenheit a Celcius
+def gradosFarenheitToCelcius(gradosFarenheit):
+    temperaturaCelcius =gradosFarenheit-32/1.8
+    return temperaturaCelcius
+
+def comprobar(mensajeEntrada):
+    if mensajeEntrada == 1:
+         grados =float(input("Dame los grados que quieres pasar: "))
+         gradosFarenheitToCelcius(grados)
+         print("{:.2f} C".format(gradosFarenheitToCelcius(grados)))
+    elif mensajeEntrada ==2:
+        grados = float(input("Dame los grados que quieres pasar: "))
+        gradosCelciusA_Farenheit(grados)
+        print("{:.2f} F".format( gradosCelciusA_Farenheit(grados)))
+    elif mensajeEntrada !=1 and mensajeEntrada !=2:
+        print("no has seleccionado ninguna opcion valida")
+
+
+print("¿Como quiere saber la temperatura?, escriba 1 o 2")
+print("1-Celsius")
+print("2-Farenheit")
+mensajeEntrada = int(input("Seleccione: "))
+comprobar(mensajeEntrada)
