e0f7b0d350ea2f7b3d0ef8cbb75298215d9c6a25
Victor <VictorCarmona9035@hotmail.com>
2023-03-20 20:32:56 +0100

    Ejercicio Cubo

--- Rectangulo.py
+++ Rectangulo.py
@@ -1,23 +1,26 @@
-class Rectangulo:
+class Cubo:
 
-    def __init__(self, b, h):
-        self.base = b
+    def __init__(self, b, h, p):
+        self.ancho = b
         self.altura = h
+        self.profundidad = p
 
     def mostra_detalles(self):
-        return self.base * self.altura
+        return self.ancho * self.altura * self.profundidad
 
 
 
 
-base  = int(input("Dame la base: "))
+base  = int(input("Dame el ancho: "))
 altura = int(input("Dame la altura: "))
+profundidad = int(input("Dame la profundidad: "))
 #Creamos el objeto
-rectangulo1 = Rectangulo(base,altura)
-print(rectangulo1.mostra_detalles())
+cubo1 = Cubo(base,altura, profundidad)
+print(cubo1.mostra_detalles())
 
 base2  = int(input("Dame la base: "))
 altura2 = int(input("Dame la altura: "))
+profundidad2 = int(input("Dame la profundidad: "))
 
-rectangulo2 =Rectangulo(base2,altura2)
-print(f'Area rectangulo2: {rectangulo2.mostra_detalles()}')
+cubo2 =Cubo(base2,altura2, profundidad2)
+print(f'Area rectangulo2: {cubo2.mostra_detalles()}')
