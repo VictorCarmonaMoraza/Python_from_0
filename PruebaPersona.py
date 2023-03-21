832ce0b868e6fd0484c3503090b4f386dff67512
Victor <VictorCarmona9035@hotmail.com>
2023-03-21 11:43:24 +0100

    108-Uso de modulos y clases en Python

---
+++ PruebaPersona.py
@@ -0,0 +1,6 @@
+from Persona import Persona
+#Si queremos importar todas las clases que se encuentran en un archivo lo hacemos de la forma
+# from Persona import *
+
+persona1 = Persona('Karla','gomez',30)
+persona1.mostrar_detalle()
